d88ce6956c3c4e01baeffbe16af73cce5eb199a8
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-04-17 16:29:18 +0200

    add components folder

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@
 
 
 function App() {
-const  name = "Najib";
-const x = true;
+
 
 
   return (
     <div className="container">
-      <h1>Hello from react</h1>
-      <h2>hello {name} {x ? "yes" : "no"}</h2>
+      
     </div>
   );
 }
