a77a7b17df51abc51cd191b02d07b16b3cf77b24
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-04-19 11:45:56 +0200

    another commit

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,6 +4,8 @@ import { FaTimes} from 'react-icons/fa';
 
 
 
+
+
  const Task = ({ task, onDelete, onToggle }) => {
   return (
     <div className={`task ${task.reminder ? 'reminder' : '' }`} onDoubleClick={ () => onToggle(task.id)}>
