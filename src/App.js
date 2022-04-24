15e24c0334ace825a94791a13f9cfc8c6b5c6e64
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-04-24 22:11:53 +0200

    fetching the task from backend(json-server) and display them.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import { useState} from 'react';
+import { useState, useEffect} from 'react'; // id order to loead the data to the page loeads we use useEffect.
 
 
 
@@ -10,9 +10,29 @@ function App() {
 
   //  another state to add button, to show the lale to add button.
   const [showAddTask, setShowAddTask] = useState(false);
-
   // thise  here is app level state.
   const [tasks, setTasks] = useState([]);
+
+
+  // By using this Hook, you tell React that your component needs to do something after render. React will remember the function you passed (we'll refer to it as our “effect”), and call it later after performing the DOM updates.
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  },[]) // we add [] (dependency array) because // if you have a value where you want this to run if that value changes you would pass it here [...] like user or somethings  we do not have any dependencies but we want to pass in just an empty array 
+
+
+  // fetch Tasks //// usng the fetch api with a sync away, to get data.
+  const fetchTasks = async () =>{
+    const res = await fetch('http://localhost:8000/tasks') // because fetch returns a promise so we want to await that promise.
+    const data = await res.json() // json-server
+
+    return data
+  }
+
+
 
 // Add Task //  that will give me a random number of id.
 const addTask = (task) => {
