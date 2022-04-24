308a7dbf8f9b7f00d231d1b32bf31e1a9494960a
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-04-24 17:09:46 +0200

    app is done version 1.0.0

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const addTask = (task) => {
 const deleteTask = (id) =>{
   setTasks(tasks.filter((task) => task.id !== id))
 }
-
+ 
 
 // Toggle reminder
 const toggleReminder = (id) => {
@@ -50,7 +50,7 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header onAdd={() => setShowAddTask (!showAddTask)}/>
+      <Header onAdd={() => setShowAddTask (!showAddTask)} showAdd={showAddTask}/>
       {showAddTask &&  <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}  onToggle={toggleReminder}/> : ' No Tasks To Show ' }
     </div>
